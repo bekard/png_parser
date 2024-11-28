faead47c1ada9a8899887f482f20af59577ea26b
Anton Arefyev <ant.ar9809@gmail.com>
2024-11-28 23:51:09 +0300

    move chunk entry to module

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,18 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
-use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
-use std::fmt;
 use std::fs::File;
 use std::io;
 use std::io::Read;
 use std::mem::size_of;
 use std::path::Path;
 
+use crate::chunk_entry::ChunkEntry;
+
+pub mod chunk_entry;
+
 const SIGNATURE: &[u8] = &[0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
 const END_TYPE_CODE: &str = "IEND";
-
-#[derive(Default, Debug)]
-struct ChunkEntry {
-    length: u32,
-    type_code: [u8; 4],
-    data: Vec<u8>,
-    crc: u32,
-}
-
-impl fmt::Display for ChunkEntry {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        const MAX_DATA_LENGTH: usize = 8;
-
-        let type_code = String::from_utf8_lossy(&self.type_code);
-        let data_fmt = if self.data.len() < MAX_DATA_LENGTH {
-            format!("Data {:?}", &self.data[..])
-        } else {
-            let data = &self.data[..MAX_DATA_LENGTH];
-            format!("First {} bytes of data: {:?}", data.len(), data)
-        };
-
-        write!(
-            f,
-            "Chunk {}, Data Length: {} Bytes, CRC: {:#x}\n{}",
-            type_code, self.length, self.crc, data_fmt
-        )
-    }
-}
-
-impl ChunkEntry {
-    fn read<T: io::Read + io::Seek>(reader: &mut T) -> Result<ChunkEntry, io::Error> {
-        let mut res: ChunkEntry = ChunkEntry::default();
-
-        res.length = reader.read_u32::<BigEndian>()?;
-        reader.read_exact(res.type_code.as_mut_slice())?;
-
-        res.data.resize(res.length as usize, 0);
-        reader.read_exact(res.data.as_mut_slice())?;
-
-        res.crc = reader.read_u32::<BigEndian>()?;
-
-        Ok(res)
-    }
-}
 
 fn main() -> Result<(), io::Error> {
     //let img_path = Path::new("Z:/prog/prob/png_parser/res/red_pixel.png");
